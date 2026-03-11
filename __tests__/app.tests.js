367a1b7d3341f42ffb41abfd3571fd376ed469c2
3llasious <quest4ella22@gmail.com>
2026-03-11 14:10:21 +0000

    added test descriptions for endpoint 1

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -3,8 +3,12 @@
 // const data = require("../db/data/test-data");
 // const request = require("supertest");
 
-describe("route", () => {
-  describe("endpoint", () => {
-    test("method", () => {});
+describe("/api/episodes/:episode_id/comments", () => {
+  describe("GET", () => {
+    test("gets all comments for a particular episode of a tv-show, when get is successful responds with status 200", () => {});
+
+    test("gets all comments for a particular episode of a tv-show in ascending order of runtime seconds", () => {});
+    test("filters comments to a 180-second window around the viewer's current position when ?t=x is provided", () => {});
+    test("if there are no comments within the 180 second time frame returns empty array and status 200", () => {});
   });
 });
