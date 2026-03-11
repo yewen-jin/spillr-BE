79001517e786f1a78b6eff2a89eb208ebc463442
yewen-jin <yewen.elsa.jin@gmail.com>
2026-03-11 14:41:28 +0000

    installed cors, added server object, changed to using server to listen in listen.js, setting up folder structure for socket

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
 const path = require("path");
 const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
 const cors = require("cors");
 
 const app = express();
+const server = createServer(app);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(cors());
 
+app.get("/socket", (req, res) => {
+  res.sendFile(join(__dirname, "../public/index.html"));
+});
+
 const { HTTP_STATUS_CODES } = require("./utils/constants.js");
 
-const { episodesRouter } = require("./routes/episodes.routes.js");
+// const { episodesRouter } = require("./routes/episodes.routes.js");
 
-app.use("/api/episodes", episodesRouter);
+// app.use("/api/episodes", episodesRouter);
 
 app.all(/(.*)/, (req, res) => {
   res.status(HTTP_STATUS_CODES.NOT_FOUND).send({ msg: "Route not found" });
 });
 
-module.exports = app;
+module.exports = { app, server };
