dd171c514c542e4fb8c0fbac8b89ad7374029286
3llasious <quest4ella22@gmail.com>
2026-03-11 14:15:29 +0000

    added imports to app.js file

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -1,12 +1,14 @@
-// const db = require("../db/connection.js");
-// const seed = require("../db/seeds/seed");
-// const data = require("../db/data/test-data");
-// const request = require("supertest");
+const db = require("../db/connection.js");
+const seed = require("../db/seed/seed");
+const data = require("../db/data/test/index");
+const request = require("supertest");
+
+beforeAll(() => seed(data)); //before all tests reseed the test database with data
+afterAll(() => db.end()); // after all close the db connectionS
 
 describe("/api/episodes/:episode_id/comments", () => {
   describe("GET", () => {
     test("gets all comments for a particular episode of a tv-show, when get is successful responds with status 200", () => {});
-
     test("gets all comments for a particular episode of a tv-show in ascending order of runtime seconds", () => {});
     test("filters comments to a 180-second window around the viewer's current position when ?t=x is provided", () => {});
     test("if there are no comments within the 180 second time frame returns empty array and status 200", () => {});
