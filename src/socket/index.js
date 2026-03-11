78d61adc84bb038110f3d40a241b5f71d43e4c7f
yewen-jin <yewen.elsa.jin@gmail.com>
2026-03-11 16:57:13 +0000

    created io and getIo function in index.js

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,18 +1,23 @@
 const { Server } = require("socket.io");
 // const commentsHandler = require("./handlers/comments.handler");
 // const usersHandler = require("./handlers/users.handler");
+let io;
 
 const initiateSocket = (server) => {
-  const io = new Server(server);
+  io = new Server(server);
   io.on("connection", (socket) => {
     console.log("A user connected!");
+
     socket.on("disconnect", () => {
       console.log("A user has disconnected");
     });
+
     socket.on("chat message", (msg) => {
       console.log("message: " + msg);
     });
   });
 };
 
-module.exports = { initiateSocket };
+const getIo = () => io;
+
+module.exports = { initiateSocket, getIo };
