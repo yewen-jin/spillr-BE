2a80a004c9d09e7fcd3a5773c85356ee121ec35e
yewen-jin <yewen.elsa.jin@gmail.com>
2026-03-12 16:36:25 +0000

    created the first test for socket in jest

--- __tests__/socket.test.js
+++ __tests__/socket.test.js
@@ -1,3 +1,39 @@
-describe("socket", () => {
-    test("test", () => {});
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+const ioc = require("socket.io-client");
+
+function waitFor(socket, event) {
+    return new Promise((resolve) => {
+        socket.once(event, resolve);
+    });
+}
+
+describe("socket connection", () => {
+    let io, serverSocket, clientSocket;
+
+    beforeAll((done) => {
+        const httpServer = createServer();
+        io = new Server(httpServer);
+        httpServer.listen(() => {
+            const port = httpServer.address().port;
+            clientSocket = ioc(`http://localhost:${port}`);
+            io.on("connection", (socket) => {
+                serverSocket = socket;
+            });
+            clientSocket.on("connect", done);
+        });
+    });
+
+    afterAll(() => {
+        io.close();
+        clientSocket.disconnect();
+    });
+
+    test("should work", (done) => {
+        clientSocket.on("hello", (arg) => {
+            expect(arg).toBe("world");
+            done();
+        });
+        serverSocket.emit("hello", "world");
+    });
 });
