547b86fbd92c906b93ff1376e7de0dde6165b91e
yewen-jin <yewen.elsa.jin@gmail.com>
2026-03-12 16:58:54 +0000

    added test functions with ackowledgement, emitWithAck and waitFor for socket

--- __tests__/socket.test.js
+++ __tests__/socket.test.js
@@ -36,4 +36,27 @@ describe("socket connection", () => {
         });
         serverSocket.emit("hello", "world");
     });
+
+    test("should work with an acknowledgement", (done) => {
+        serverSocket.on("hi", (cb) => {
+            cb("hola");
+        });
+        clientSocket.emit("hi", (arg) => {
+            expect(arg).toBe("hola");
+            done();
+        });
+    });
+
+    test("should work with emitWithAck()", async () => {
+        serverSocket.on("foo", (cb) => {
+            cb("bar");
+        });
+        const result = await clientSocket.emitWithAck("foo");
+        expect(result).toBe("bar");
+    });
+
+    test("should work with waitFor()", () => {
+        clientSocket.emit("baz");
+        return waitFor(serverSocket, "baz");
+    });
 });
