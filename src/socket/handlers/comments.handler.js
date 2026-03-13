6e3498d00f0579c77cfd16f6cd685fa13c15932b
yewen-jin <yewen.elsa.jin@gmail.com>
2026-03-13 12:21:09 +0000

    added first comment

--- src/socket/handlers/comments.handler.js
+++ src/socket/handlers/comments.handler.js
@@ -1,40 +1,47 @@
-// const db = require("../../../db/connection.js");
 const { getIo } = require("../index.js");
 const {
-  addReaction,
-  removeReaction,
-  addReply,
-  deleteReply,
-  deleteComment,
-  insertComment,
-  patchSpoiler,
+    addReaction,
+    removeReaction,
+    addReply,
+    deleteReply,
+    deleteComment,
+    insertComment,
+    patchSpoiler,
 } = require("../models/comment.models.js");
 
 const commentsHandler = () => {
-  const io = getIo();
-  io.on("connection", (socket) => {
-    console.log("commentsHandler got connected!");
+    const io = getIo();
+    io.on("connection", (socket) => {
+        console.log("commentsHandler got connected!");
 
-    socket.on("chat message", (msg) => {
-      console.log(">---writing to database--->");
-    });
+        socket.on("chat message", (msg) => {
+            console.log(">---writing to database--->");
+        });
 
-    socket.on("comment", (comment) => {
-      console.log(`received comment for episode ${comment.episode_id}`);
-      io.emit("comment", comment);
-      insertComment(comment);
-    });
+        socket.on("comment body", (commentBody) => {
+            console.log(`received comment`);
+            const comment = {};
+            // episode_id, body, user_id, runtime_seconds, is_spoiler
+            comment.body = commentBody;
+            comment.episode_id = 469092;
+            comment.user_id = "a1b2c3d4-e5f6-7890-abcd-ef1234567890";
+            comment.runtime_seconds = 1;
+            comment.is_spoiler = false;
+            console.log(comment);
+            io.emit("comment", comment);
+            insertComment(comment);
+        });
 
-    socket.on("reply", (reply) => {
-      console.log(`received reply for comment ${reply.comment_id}`);
-      io.emit("reply", reply);
-    });
+        socket.on("reply", (reply) => {
+            console.log(`received reply for comment ${reply.comment_id}`);
+            io.emit("reply", reply);
+        });
 
-    socket.on("reaction", (reaction) => {
-      console.log(`received reaction ${reaction.reaction_type} `);
-      io.emit("reaction", reaction);
+        socket.on("reaction", (reaction) => {
+            console.log(`received reaction ${reaction.reaction_type} `);
+            io.emit("reaction", reaction);
+        });
     });
-  });
 };
 
 module.exports = commentsHandler;
--- src/socket/models/comment.models.js
+++ src/socket/models/comment.models.js
@@ -1,146 +1,147 @@
-const db = require("../../db/connection.js");
+const db = require("../../../db/connection.js");
 const {
-  isLive,
-  doesThisCommentExist,
-  doesThisReplyExist,
-  doesThisReactionExist,
+    isLive,
+    doesThisCommentExist,
+    doesThisReplyExist,
+    doesThisReactionExist,
 } = require("./utils.js");
 const { NotFoundError } = require("../../errors/customError.js");
 
 const insertComment = async (commentObj) => {
-  const { episode_id, body, user_id, runtime_seconds, is_spoiler } = commentObj;
-  if (!is_spoiler) {
-    const is_live = await isLive(episode_id);
-    const { rows } = await db.query(
-      `INSERT INTO comments
-    (user_id , body, episode_id, runtime_seconds, is_live) VALUES 
+    const { episode_id, body, user_id, runtime_seconds, is_spoiler } =
+        commentObj;
+    if (!is_spoiler) {
+        const is_live = await isLive(episode_id);
+        const { rows } = await db.query(
+            `INSERT INTO comments
+    (user_id , body, episode_id, runtime_seconds, is_live) VALUES
     ($1,$2,$3,$4,$5) RETURNING *`,
-      [user_id, body, episode_id, runtime_seconds, is_live],
-    );
-    return rows[0];
-  } else {
-    const is_live = await isLive(episode_id);
-    const { rows } = await db.query(
-      `INSERT INTO comments
-    (user_id , body, episode_id, runtime_seconds, is_live, is_spoiler) VALUES 
+            [user_id, body, episode_id, runtime_seconds, is_live],
+        );
+        return rows[0];
+    } else {
+        const is_live = await isLive(episode_id);
+        const { rows } = await db.query(
+            `INSERT INTO comments
+    (user_id , body, episode_id, runtime_seconds, is_live, is_spoiler) VALUES
     ($1,$2,$3,$4,$5,$6) RETURNING *`,
-      [user_id, body, episode_id, runtime_seconds, is_live, is_spoiler],
-    );
-    return rows[0];
-  }
-  // also need to error handle a reply with no body, user_id, no runtime-seconds, no episode id
+            [user_id, body, episode_id, runtime_seconds, is_live, is_spoiler],
+        );
+        return rows[0];
+    }
+    // also need to error handle a reply with no body, user_id, no runtime-seconds, no episode id
 };
 
 const patchSpoiler = async (comment_id, is_spoiler) => {
-  if (await doesThisCommentExist(comment_id)) {
-    const { rows } = await db.query(
-      `UPDATE comments SET is_spoiler = $1 WHERE comment_id = $2 RETURNING *`,
-      [is_spoiler, comment_id],
-    );
-    return rows[0];
-  }
-  throw new NotFoundError(`Comment with id ${comment_id} not found`);
+    if (await doesThisCommentExist(comment_id)) {
+        const { rows } = await db.query(
+            `UPDATE comments SET is_spoiler = $1 WHERE comment_id = $2 RETURNING *`,
+            [is_spoiler, comment_id],
+        );
+        return rows[0];
+    }
+    throw new NotFoundError(`Comment with id ${comment_id} not found`);
 };
 
 const deleteComment = async (comment_id) => {
-  if ((await doesThisCommentExist(comment_id)) === true) {
-    const { rows } = await db.query(
-      `DELETE FROM comments
+    if ((await doesThisCommentExist(comment_id)) === true) {
+        const { rows } = await db.query(
+            `DELETE FROM comments
      WHERE comment_id = $1
      RETURNING *`,
-      [comment_id],
-    );
+            [comment_id],
+        );
 
-    return rows[0];
-  } else {
-    throw new NotFoundError(`Comment with id ${comment_id} not found`);
-  }
+        return rows[0];
+    } else {
+        throw new NotFoundError(`Comment with id ${comment_id} not found`);
+    }
 };
 
 const addReply = async (replyObj) => {
-  const { comment_id, episode_id, user_id, body, runtime_seconds } = replyObj;
-  if ((await doesThisCommentExist(comment_id)) === true) {
-    const { rows } = await db.query(
-      `INSERT INTO replies
-    (comment_id , user_id, body, episode_id, runtime_seconds) VALUES 
+    const { comment_id, episode_id, user_id, body, runtime_seconds } = replyObj;
+    if ((await doesThisCommentExist(comment_id)) === true) {
+        const { rows } = await db.query(
+            `INSERT INTO replies
+    (comment_id , user_id, body, episode_id, runtime_seconds) VALUES
     ($1,$2,$3,$4,$5) RETURNING *`,
-      [comment_id, episode_id, user_id, body, runtime_seconds],
-    );
-    return rows[0];
-  } else {
-    throw new NotFoundError(`Comment with id ${comment_id} not found`);
-    // also need to error handle a reply with no body, user_id, no runtime-seconds, no episode id
-  }
+            [comment_id, episode_id, user_id, body, runtime_seconds],
+        );
+        return rows[0];
+    } else {
+        throw new NotFoundError(`Comment with id ${comment_id} not found`);
+        // also need to error handle a reply with no body, user_id, no runtime-seconds, no episode id
+    }
 };
 
 const deleteReply = async (reply_id) => {
-  if ((await doesThisReplyExist(reply_id)) === true) {
-    const { rows } = await db.query(
-      `DELETE FROM replies
+    if ((await doesThisReplyExist(reply_id)) === true) {
+        const { rows } = await db.query(
+            `DELETE FROM replies
      WHERE reply_id = $1
      RETURNING *`,
-      [reply_id],
-    );
+            [reply_id],
+        );
 
-    return rows[0];
-  } else {
-    throw new NotFoundError(`Reply with id ${reply_id} not found`);
-  }
+        return rows[0];
+    } else {
+        throw new NotFoundError(`Reply with id ${reply_id} not found`);
+    }
 };
 const addReaction = async (reactionObj) => {
-  const {
-    reaction_type,
-    comment_id,
-    episode_id,
-    reply_id,
-    runtime_seconds,
-    user_id,
-  } = reactionObj;
-
-  if (!episode_id && !comment_id && !reply_id) {
-    throw new NotFoundError(
-      "A reaction must target an episode, comment, or reply",
-    );
-  } else {
-    const { rows } = await db.query(
-      `INSERT INTO reactions
-    (reaction_type, comment_id, episode_id, reply_id, runtime_seconds, user_id) VALUES 
-    ($1,$2,$3,$4,$5,$6) RETURNING *`,
-      [
+    const {
         reaction_type,
-        comment_id ?? null,
-        episode_id ?? null,
-        reply_id ?? null,
+        comment_id,
+        episode_id,
+        reply_id,
         runtime_seconds,
         user_id,
-      ],
-    );
+    } = reactionObj;
 
-    return rows[0];
-  }
+    if (!episode_id && !comment_id && !reply_id) {
+        throw new NotFoundError(
+            "A reaction must target an episode, comment, or reply",
+        );
+    } else {
+        const { rows } = await db.query(
+            `INSERT INTO reactions
+    (reaction_type, comment_id, episode_id, reply_id, runtime_seconds, user_id) VALUES
+    ($1,$2,$3,$4,$5,$6) RETURNING *`,
+            [
+                reaction_type,
+                comment_id ?? null,
+                episode_id ?? null,
+                reply_id ?? null,
+                runtime_seconds,
+                user_id,
+            ],
+        );
+
+        return rows[0];
+    }
 };
 
 const removeReaction = async (reaction_id) => {
-  if ((await doesThisReactionExist(reaction_id)) === true) {
-    const { rows } = await db.query(
-      `DELETE FROM reactions
+    if ((await doesThisReactionExist(reaction_id)) === true) {
+        const { rows } = await db.query(
+            `DELETE FROM reactions
      WHERE reaction_id = $1
      RETURNING *`,
-      [reaction_id],
-    );
+            [reaction_id],
+        );
 
-    return rows[0];
-  } else {
-    throw new NotFoundError(`Reaction with id ${reaction_id} not found`);
-  }
+        return rows[0];
+    } else {
+        throw new NotFoundError(`Reaction with id ${reaction_id} not found`);
+    }
 };
 
 module.exports = {
-  addReaction,
-  removeReaction,
-  addReply,
-  deleteReply,
-  deleteComment,
-  insertComment,
-  patchSpoiler,
+    addReaction,
+    removeReaction,
+    addReply,
+    deleteReply,
+    deleteComment,
+    insertComment,
+    patchSpoiler,
 };
