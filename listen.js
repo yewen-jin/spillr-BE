7b28ad86ca86a3def2303277316100b9e4c5e286
Ines Cadete <inesmcadete@hotmail.com>
2026-03-17 16:13:29 +0000

    updated the get user by id and username endpoints

--- listen.js
+++ listen.js
@@ -1,5 +1,5 @@
 const { server } = require("./src/app.js");
-const { PORT = 9092 } = process.env;
+const { PORT = 9093 } = process.env;
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}...`);
