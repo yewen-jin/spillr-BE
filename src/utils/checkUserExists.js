07f960c2fd89f65bae5e439c1e93851510668396
3llasious <quest4ella22@gmail.com>
2026-03-22 08:34:00 +0000

    fixed incorrect SQL and no connection import

--- src/utils/checkUserExists.js
+++ src/utils/checkUserExists.js
@@ -1,9 +1,12 @@
-const checkUserExists = async (db, user_id) => {
+const db = require("../../db/connection.js");
+const { NotFoundError } = require("../errors/customError.js");
+
+const checkUserExists = async (user_id) => {
   const { rows } = await db.query(
-    "SELECT user_id FROM users WHERE user_id = $1",
+    "SELECT user_id FROM profiles WHERE user_id = $1",
     [user_id],
   );
-  if (!rows.length) throw new AppError("User not found", 404);
+  if (!rows.length) throw new NotFoundError("User not found");
   return rows[0];
 };
 
